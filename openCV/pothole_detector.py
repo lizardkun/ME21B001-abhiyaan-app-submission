d417fdaed5a6c39feec8b38af8406221210d174e
aahana18 <94188928+aahana18@users.noreply.github.com>
2022-04-20 13:20:01 +0530

    Update pothole_detector.py

--- openCV/pothole_detector.py
+++ openCV/pothole_detector.py
@@ -12,19 +12,22 @@ ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 print(frame1.shape)
 while cap.isOpened():
+    #masking the video
     gray = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
     ret,gray = cv2.threshold(gray,127,255,0)
     gray2 = gray.copy()
     mask = np.zeros(gray.shape,np.uint8)
     contours, hier = cv2.findContours(gray,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    #checking for contours in the masked video
     for cnt in contours:
-        if 100<cv2.contourArea(cnt)<700:
-            (x, y, w, h) = cv2.boundingRect(cnt)
+        if 200<cv2.contourArea(cnt)<3000:
+            #drawing the potholes
             cv2.drawContours(frame1,[cnt],0,(0,255,0),2)
             cv2.drawContours(mask,[cnt],0,255,-1)
+            
             if cv2.contourArea(cnt) < 500:
                 continue
-            cv2.rectangle(mask, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            
     cv2.imshow("feed", frame1)
     frame1 = frame2
     ret, frame2 = cap.read()
